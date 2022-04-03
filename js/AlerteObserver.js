6e51aa58635a24fa0cf5d0435f7ae54af640d126
Leo-Pellegrin <leo.pellegrin@etu.univ-amu.fr>
2022-04-03 22:53:56 +0200

    Rangement

--- js/AlerteObserver.js
+++
@@ -1,9 +0,0 @@
-class AlerteObserver{
-    constructor(elementId){
-        this.element = document.getElementById(elementId)
-    }
-
-    update(model, alerte){
-        this.element.innerHTML = model.alerte;
-    }
-}
